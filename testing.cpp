03be799973842bb8b8feb13a5af1b6f0cd6cc344
HaskizMe <haskellb2017@gmail.com>
2023-01-20 17:26:33 -0700

    updating

--- testing.cpp
+++ testing.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main(){
-string i;
-cin >> i;
 
-while(cin >> i){
-    cout << "you entered " << i << endl;
-}
+int main() {
+
+    string firstName;
+    string middleName;
+    string lastName;
+
+    cin >> firstName >> middleName >> lastName;
+    if(middleName.empty() || firstName.empty() || lastName.empty()){
+        cout << lastName << ", " << firstName[0] << "." << endl;
+
+    }
+    else{
+        cout << lastName << ", " << firstName[0] << "." << middleName[0] << "." << endl;
+
+    }
+
+   return 0;
 }
