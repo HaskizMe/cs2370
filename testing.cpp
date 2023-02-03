e52206182387e34691ea0aeae9d450c133b2d1ca
HaskizMe <haskellb2017@gmail.com>
2023-02-03 13:54:26 -0700

    working on lab 6.43

--- testing.cpp
+++ testing.cpp
@@ -1,157 +1,70 @@
-
-
-
-//close to working
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <vector> 
+#include <string>
+#include <cctype>
+#include <bits/stdc++.h>
 using namespace std;
 
-    vector<int> ratings;
-    vector<int> jersey_numbers;
+ int GetWordFrequency(vector<string> wordsList, string currWord){
+    int count=1;
+    //outer loop will loop through number of elements in the list
+    int j = 1;
+        //Inner loop comparing words
+        if(wordsList[0] == currWord){
 
-    vector<int> get_ratings(vector<int> ratings){
-        return ratings;
-    }
-    vector<int> get_jersey_numbers(vector<int> jersey_numbers){
-        return jersey_numbers;
-    }
-
-    void add_player(){
-        int num;
-        int rating;
-        cout << "Enter a new player's jersey number:" << endl;
-        cin >> num;
-        jersey_numbers.push_back(num);
-        cout << "Enter the player's rating:" << endl;
-        cin >> rating;
-        ratings.push_back(rating);
-
-    return;
-    }
-    void output_roster(){
-        cout << "ROSTER" << endl;
-        int j = 0;
-        for(int i = 0; i < jersey_numbers.size(); i++){
-            cout << "Player " << j+1 << " -- Jersey number: " << jersey_numbers[i] << ", Rating: " << ratings[i] << endl; 
-            j++;
-        }
-        return;
-    }
-    void remove_player(){
-        int match;
-        int index = 0;
-        vector<int>::iterator it;
-        cout << "Enter a jersey number:" << endl;
-        cin >> match;
-        for(int i = 0; i < ratings.size(); i++){
-            if(match == jersey_numbers[i]){
-                index = i;
-                ratings.erase(ratings.begin()+index);
-                jersey_numbers.erase(jersey_numbers.begin()+index);
+        for(j; j < wordsList.size(); j++){
+            //comparing two strings
+            if(currWord == wordsList[j]){
+                count++;
             }
-         }
-        output_roster();
-
-        return;
-
-    }
-    void update_rating(){
-        int jersey;
-        int rating;
-        int index;
-        cout << "Enter a jersey number:" << endl;
-        cin >> jersey;
-        cout << "Enter a new rating for player:" << endl;
-        cin >> rating;
-        for(int i = 0; i<ratings.size(); i++){
-            if(jersey_numbers[i] == jersey){
-                index = i;
+           
+        }
+        }
+        else{
+            count = 0;
+            for(j; j < wordsList.size(); j++){
+            //comparing two strings
+            if(currWord == wordsList[j]){
+                count++;
             }
         }
-        replace(ratings.begin(),ratings.end(),ratings[index],rating );
-            for (int x : ratings){
-        cout << x << " ";
-
-    }
-    }
-
-    void above_rating(){
-        int input;
-        int j = 0;
-        cout << "Enter a rating:" << endl;
-        cin >> input;
-        cout << "ABOVE " << input << endl;
-
-        for(int i = 0; i < ratings.size(); i++){
-            if(ratings[i] > input){
-                cout << "Player " << i+1 << " -- Jersey number: " << jersey_numbers[i] << ", Rating: " << ratings[i] << endl;
-                j++;
-            }
         }
+    return count;
+ }
 
-        return;
-    }
-
-
-    void menu(vector<int> ratings, vector<int> jersey_numbers){
-        char input;
-        do {
-        cout << "" << endl;
-        cout << "MENU" << endl;
-        cout << "a - Add player\nd - Remove player\nu - Update player rating\nr - Output players above a rating\no - Output roster\nq - Quit\n" << endl;
-        cout << "Choose an option:" << endl;
-        cin >> input;
-
-        
-        switch (input)
-        {
-        case 'a':
-        //working
-            add_player();
-            break;
-        case 'd':
-        //working
-            remove_player();
-            break;
-        case 'u':
-        //working
-            update_rating();
-            break;
-        case 'r':
-            //method
-            above_rating();
-            break;
-        case 'o':
-            output_roster();
-            break;
-        case 'q':
-            //method
-            break;
-
-        default:
-            cout << "Please enter a valid letter" << endl;
-            break;
-        }
-        } while(input != 'q');
-    }
 int main() {
 
-    int jersey;
-    int rating;
-    int j = 0;
-    for(int i = 0; i < 5; i++){
-        cout << "Enter player " << j+1 << "'s jersey number:" << endl;
-        cin >> jersey;
-        jersey_numbers.push_back(jersey);
-        cout << "Enter player " << j+1 << "'s rating:" << endl;
-        cin >> rating;
-        cout << "" << endl;
-        ratings.push_back(rating);
-        j++;
+    vector<string> input_words;
+
+    string input;
+    int number_of_words;
+
+    cin >> number_of_words;
+
+    getline(cin,input);
+
+    istringstream ss(input);
+    string word;
+
+    //Seperating words by spaces
+    while (ss >> word)
+    {
+        input_words.push_back(word);
+        
+    }
+        vector<string> input_words_lower_case(input_words);
+
+    //changing all letters to lower case
+    for(int i = 0; i<input_words.size();i++){
+        transform(input_words_lower_case[i].begin(), input_words_lower_case[i].end(), input_words_lower_case[i].begin(), ::tolower);
     }
 
-    output_roster();
-    menu(ratings, jersey_numbers);
+    for(int k = 0; k < number_of_words; k++ )
+    {
+        string currWord = input_words_lower_case[k];
+        string show_word = input_words[k];
+        cout << show_word << " " << GetWordFrequency(input_words_lower_case,currWord) << endl;
+    }
+
    return 0;
 }
--- word_frequencies.cpp
+++ word_frequencies.cpp
@@ -5,33 +5,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- int GetWordFrequency(vector<string> wordsList, string& currWord){
-    int count=0;
+ int GetWordFrequency(vector<string> wordsList, string currWord){
+    
+     for(unsigned int i = 0; i<wordsList.size();i++){
+        transform(wordsList[i].begin(), wordsList[i].end(), wordsList[i].begin(), ::tolower);
+    }
+    int count = 0;
     //outer loop will loop through number of elements in the list
-    for(int i;i<wordsList.size(); i++){
+    // int j = 1;
+        //Inner loop comparing words
+        // if(wordsList[0] == currWord){
 
-        //Inner loop will output the number of frequency of words
-        for(int j = 1; j < wordsList.size(); j++){
+        for(unsigned int j = 0; j < wordsList.size(); j++){
             //comparing two strings
-            if(wordsList[i] == wordsList[j]){
+            if(currWord == wordsList[j]){
                 count++;
             }
-            return count;
+           
         }
-
-
-    }
-    return 0;
+        // }
+        // else{
+        //     count = 0;
+        //     for(int k = 1; k < wordsList.size(); k++){
+        //     //comparing two strings
+        //     if(currWord == wordsList[j]){
+        //         count++;
+        //     }
+        // }
+        // }
+    return count;
  }
 
 int main() {
 
     vector<string> input_words;
+
     string input;
+    int number_of_words;
+
+    cin >> number_of_words;
 
     getline(cin,input);
-    // cin >> input;
-    // input_words.push_back(input);
 
     istringstream ss(input);
     string word;
@@ -40,20 +54,19 @@ int main() {
     while (ss >> word)
     {
         input_words.push_back(word);
+        
     }
     //changing all letters to lower case
-    for(int i = 0; i<input_words.size();i++){
-        transform(input_words[i].begin(), input_words[i].end(), input_words[i].begin(), ::tolower);
+    // for(int i = 0; i<input_words.size();i++){
+    //     transform(input_words_lower_case[i].begin(), input_words_lower_case[i].end(), input_words_lower_case[i].begin(), ::tolower);
+    // }
+
+    for(int k = 0; k < number_of_words; k++)
+    {
+        string currWord = input_words[k];
+        cout << currWord << " " << GetWordFrequency(input_words,currWord) << endl;
+        // cout<< GetWordFrequency(input_words, currWord) << endl;
     }
-    //printing all elements in vector
-    for(auto x : input_words){
-        cout<< x<< endl;
-    }
-// string s2 = input_words[0];
-//    transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
-//    cout<<s2<<endl;
-    string currWord = input_words[0];
-    GetWordFrequency(input_words,currWord);
 
    return 0;
 }
