8195d66fef013e54f5b70c45fbf1b6a43b731e4c
HaskizMe <haskellb2017@gmail.com>
2023-02-09 18:13:46 -0700

    working on lab 7.11

--- number_patter.cpp
+++ number_patter.cpp
@@ -5,40 +5,47 @@ using namespace std;
 void PrintNumPattern(int num1, int num2){
     static int NUMBER = 12;
     int currentNumber = num1;
-    // if(currentNumber < 0 - num2){
-    //     if(currentNumber == 12){
-    //         cout << "hi " <<num1;
+    int count = 0;
 
-            
-    //         return;
-    //     }
-    //     else{
-    //         currentNumber = currentNumber + num2;
-    //         cout << currentNumber;
-    //         PrintNumPattern(currentNumber, num2);
-    //     }
-    // }
-    // else{
-    // cout << currentNumber << " ";
-    // currentNumber = num1 - num2;
-    // PrintNumPattern(currentNumber, num2);
-    // }
-
-    if(currentNumber >= -3){
-        cout << currentNumber << " ";
-        currentNumber = currentNumber - num2;
-        PrintNumPattern(currentNumber, num2);
-    }
-
-    else{
-        cout << currentNumber << " ";
-        currentNumber = currentNumber + num2;
-        PrintNumPattern(currentNumber, num2);
-        if(currentNumber == 12){
-            cout << currentNumber;
+    //use a counter to count how many times 
+    //it iterates to get to a negative and then 
+    //use that variable to make a loop and base case 
+    //to know how many times it needs to add again
+    if(currentNumber < 0){
+        if(currentNumber == num1){
+            // cout << "hi " <<num1;        
             return;
         }
+        else{
+            currentNumber = currentNumber + num2;
+            cout << currentNumber;
+            PrintNumPattern(currentNumber, num2);
+        }
     }
+    else{
+    cout << currentNumber << " ";
+    currentNumber = num1 - num2;
+    // count++;
+    // cout<<count;
+    PrintNumPattern(currentNumber, num2);
+
+    }
+
+    // if(currentNumber >= 0){
+    //     cout << currentNumber << " ";
+    //     currentNumber = currentNumber - num2;
+    //     PrintNumPattern(currentNumber, num2);
+    // }
+
+    // else{
+    //     cout << currentNumber << " ";
+    //     currentNumber = currentNumber + num2;
+    //     PrintNumPattern(currentNumber, num2);
+    //     if(currentNumber == 12){
+    //         cout << currentNumber;
+    //         return;
+    //     }
+    // }
 
 
 }
