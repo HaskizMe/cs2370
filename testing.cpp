b18b65d74cdf1ec25efef7d9144c5be3507c82d2
HaskizMe <haskellb2017@gmail.com>
2023-04-18 16:42:39 -0600

    from old computer

--- testing.cpp
+++ testing.cpp
@@ -1,8 +1,15 @@
+#include <vector>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <iomanip>
+#include <list>
+#include <algorithm>
 using namespace std;
-
-int main(int argc, char* argv[]) {
-   cout << 2%2 << endl;
-
+int main() {
+    vector<string> my_vector_items_txt;
+    my_vector_items_txt.push_back("hello");
+    cout << my_vector_items_txt.at(0) << endl;
+    cout << "Hello World!";
+    return 0;
 }
