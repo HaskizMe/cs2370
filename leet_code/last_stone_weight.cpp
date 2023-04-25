ff162b540d7c806bd891048763e5e9e674ef933f
Bailey Haskell <Bailey Haskell>
2023-04-24 18:35:43 -0600

    pushing

---
+++ leet_code/last_stone_weight.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// class Solution {
+// public:
+//     int lastStoneWeight(vector<int>& stones) {
+        
+//         for(int i = 0; i < stones.size(); i++)
+//         {
+
+//         }
+//     }
+// };
+int main()
+{
+    vector<int>& stones;
+    stones.push_back(5);
+    cout << stones[0];
+
+}
