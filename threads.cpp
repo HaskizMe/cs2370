243afbf65910d378a00b862d890f27bbc381c197
Bailey Haskell <Bailey Haskell>
2023-04-29 17:03:56 -0600

    corrected

--- threads.cpp
+++ threads.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <thread>
 #include <mutex>
-using namespace std;
+#include <vector>
+#include <algorithm>
 
-mutex mtx;  // mutex to protect shared resources
+std::mutex mtx;  // mutex to protect shared resources
 
-int total_primes = 0;  // total number of primes found
-vector<int> primes_per_thread(4, 0);  // number of primes found by each thread
+int total_primes = 0;
+std::vector<int> primes_found(4, 0);
 
 bool is_prime(int n) {
-    if (n <= 1) {
+    if (n < 2) {
         return false;
     }
-    if (n == 2) {
-        return true;
-    }
-    if (n % 2 == 0) {
-        return false;
-    }
-    for (int i = 3; i <= n / 2; i += 2) {
+    for (int i = 2; i*i <= n; i++) {
         if (n % i == 0) {
             return false;
         }
@@ -28,34 +22,34 @@ bool is_prime(int n) {
     return true;
 }
 
-void find_primes(int thread_id) {
-    int num = thread_id + 2;
-    while (num <= 2000000) {
-        if (is_prime(num)) {
-            lock_guard<mutex> lock(mtx);  // lock the mutex
-            ofstream out("primes.dat", std::ios::app);
-            out << num << "\n";  // write to file
-            out.close();
-            total_primes++;  // update total count
-            primes_per_thread[thread_id]++;  // update count for this thread
+void find_primes(int thread_num, int start_num, int end_num) {
+    for (int i = start_num; i <= end_num; i++) {
+        if (is_prime(i)) {
+            std::lock_guard<std::mutex> lock(mtx);
+            std::ofstream outfile("primes.dat", std::ios_base::app);
+            outfile << i << std::endl;
+            total_primes++;
+            primes_found[thread_num]++;
         }
-        num += 4;  // move to next number that this thread should check
     }
 }
 
 int main() {
-    ofstream out("primes.dat");
-    out.close();  // create an empty file
-    vector<thread> threads;
+    std::vector<std::thread> threads;
+    int start_num = 2;
+    int end_num = 1000000;
+    int subrange_size = (end_num - start_num + 1) / 4;
     for (int i = 0; i < 4; i++) {
-        threads.emplace_back(find_primes, i);  // create and start threads
+        int subrange_start = start_num + i * subrange_size;
+        int subrange_end = (i == 3) ? end_num : subrange_start + subrange_size - 1;
+        threads.emplace_back(find_primes, i, subrange_start, subrange_end);
     }
-    for (auto& thread : threads) {
-        thread.join();  // wait for all threads to finish
+    for (auto& t : threads) {
+        t.join();
     }
-    cout << "Total primes found: " << total_primes << "\n";
+    std::cout << "Total primes found: " << total_primes << std::endl;
     for (int i = 0; i < 4; i++) {
-        cout << "Thread " << i+1 << " found " << primes_per_thread[i] << " primes\n";
+        std::cout << "Thread " << i+1 << " found " << primes_found[i] << " primes." << std::endl;
     }
     return 0;
 }
